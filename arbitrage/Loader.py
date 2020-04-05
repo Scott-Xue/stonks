103d6803d1202e81591cac91574de8d22d41204a
Scott Xue <scottxue@berkeley.edu>
2020-04-04 19:51:10 -0700

    pr 2

---
+++ arbitrage/Loader.py
@@ -0,0 +1,8 @@
+'''Loads the given csv file into a usable Python object'''
+
+class Loader(object):
+    '''class to contain data from path passed in to command line'''
+    def __init__(self, csv):
+        self.data = csv
+
+
---
+++ arbitrage/__main__.py
@@ -0,0 +1,7 @@
+import sys
+
+
+'''Starts the program'''
+if __name__ == '__main__':
+    args = sys.argv[1]
+    print("Hello World!")
