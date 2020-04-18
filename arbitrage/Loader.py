4134378d2bbdd8db9ed1d827233cc2990199d175
Scott Xue <scottxue@berkeley.edu>
2020-04-18 12:01:48 -0700

    fixed issues

--- arbitrage/Loader.py
+++ arbitrage/Loader.py
@@ -1,6 +1,6 @@
-import csv
-
 """Reads the given csv file into a list of strings containing the names of the securities."""
+
+import csv
 
 def load(file):
     with open(file) as f:
--- tests/loaderTest.py
+++ tests/loaderTest.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         parent_dir = sys.path[0]
         stocks = loader.load(parent_dir + '/tests/test1.csv')
-        self.assertEqual(stocks, ['one', 'two', 'three'])
+        self.assertEqual(stocks, ['NASDAQ:AAPL', 'NASDAQ:EA', 'NSYE:AXE'])
 
 if __name__ == '__main__':
     unittest.main()
