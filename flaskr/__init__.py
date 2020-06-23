79b6a11c6df5de0e6bf7f045251757f3c043fdd7
Scott Xue <scottxue@berkeley.edu>
2020-06-22 21:26:21 -0700

    added redirect

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from src import arbitrage
 
 
@@ -6,13 +6,16 @@ def create_app(test_config=None):
     # flask server
     app = Flask(__name__, instance_relative_config=True)
 
-    @app.route('/', methods=("GET", "POST"))
-    def print_opportunities():
+    @app.route('/')
+    def index():
         ticker = request.args.get('ticker')
         if ticker:
-            q = arbitrage.Query(stock_names=[ticker])
-            opportunities = q.find_opportunities()
-            return str(opportunities)
+            return redirect(url_for('.print_opportunities', stock_name=ticker))
         return render_template("base.html")
 
+    @app.route('/stock/<stock_name>')
+    def print_opportunities(stock_name):
+        q = arbitrage.Query(stock_names=[stock_name])
+        opportunities = q.find_opportunities()
+        return str(opportunities)
     return app
